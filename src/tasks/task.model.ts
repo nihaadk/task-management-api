67fc7705de7f0bbd57341b8bd00776bedb51e34a
nihker <nihadkeric@gmail.com>
2020-02-29 23:45:00 +0100

    Create createTask method in service and install uuid npm package

--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -1,8 +1,15 @@
-export interface Task {
-	id: number;
+export class Task {
+	id: string;
 	title: string;
 	description: string;
 	status: TaskStatus;
+
+	constructor(_id: string, _title: string, _description: string, _status: TaskStatus) {
+		this.id = _id;
+		this.title = _title;
+		this.description = _description;
+		this.status = _status;
+	}
 }
 
 export enum TaskStatus {
