3a01520a8d4e0ee70465110642ae188be0e3f53d
nihker <nihadkeric@gmail.com>
2020-03-03 21:30:12 +0100

    Crated UserRepository and inject in the AuthService

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+
+
+@Injectable()
+export class AuthService {
+
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository) {
+
+    }
+}
